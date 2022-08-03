a13230531ea3c3872be436fea698aa97c69791a0
ThomasVrhydn <80472867+ThomasVrhydn@users.noreply.github.com>
2022-08-03 15:49:02 +0200

    Update and rename CheckAzureVMExtensions.ps1 to CheckAzureVMExtensionsMDE.Windows.ps1

---
+++ CheckAzureVMExtensionsMDE.Windows.ps1
@@ -0,0 +1,20 @@
+﻿Connect-AzAccount
+
+
+$ALLVMs = Get-AzVM
+foreach ($VM in $ALLVMs){
+$Extensions = Get-AzVMExtension -ResourceGroupName $VM.ResourceGroupName -VMName $VM.Name -Name "MDE.Windows"
+if($Extensions.ProvisioningState -eq "Provision Failed"){
+Remove-AzVMExtension -ResourceGroupName $VM.ResourceGroupName -Name "MDE.Windows" -VMName $VM.Name
+}
+}
+
+
+$ALLVMs = Get-AzVM
+foreach ($VM in $ALLVMs){
+$Extensions = Get-AzVMExtension -ResourceGroupName $VM.ResourceGroupName -VMName $VM.Name -Name "MDE.Windows"
+write-host $VM.Name 
+write-host $Extensions.ProvisioningState
+}
+
+
